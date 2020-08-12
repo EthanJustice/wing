c9f89f405b0cfc21be27de468e49bf188d875b51
EthanJustice <47954690+EthanJustice@users.noreply.github.com>
2020-08-12 15:54:00 -0400

    chore: config tests

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,10 @@ use std::path::Path;
 
 // external
 use serde::{Deserialize, Serialize};
-use serde_json::{from_str, Error};
+use serde_json::from_str;
 
 #[derive(Deserialize, Serialize, Debug)]
+#[serde(rename_all = "camelCase")]
 pub struct WingConfig {
     pub rss: bool,
     pub site_map: bool,
@@ -32,16 +33,38 @@ impl WingConfig {
             "\\wing.json"
         )));
 
-        if let Ok(value) = config_raw {
-            let config: WingConfig = from_str(value.as_str())?;
+        match config_raw {
+            Ok(value) => {
+                let config: WingConfig = from_str(value.as_str())?;
 
-            Ok(config)
-        } else if let Err(e) = config_raw {
-            Err(e)
-        } else {
-            Ok(WingConfig {
-                ..Default::default()
-            })
+                Ok(config)
+            }
+            Err(e) => Err(e),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::WingConfig;
+    #[test]
+    pub fn test_config() {
+        let from_fn = WingConfig::new();
+
+        match from_fn {
+            Ok(val) => {
+                let defaults = WingConfig {
+                    ..Default::default()
+                };
+
+                assert_eq!(defaults.rss, val.rss);
+                assert_eq!(defaults.site_map, val.site_map);
+                assert_eq!(defaults.link_type, val.link_type);
+                assert_eq!(defaults.optimisation_level, val.optimisation_level);
+            }
+            Err(e) => {
+                assert_eq!(true, false, "WingConfig::new() failed: {}", e);
+            }
         }
     }
 }
